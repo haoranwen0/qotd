392d942973685f885605455af94bea2bd05b9789
Evan Seeyave <evan.seeyave@gmail.com>
2024-11-25 17:04:57 -0500

    Finished answer feed

--- src/components/AnswerFeed/hook.ts
+++ src/components/AnswerFeed/hook.ts
@@ -52,6 +52,8 @@ export const useAnswerFeed = (): UseAnswerFeedResults => {
     //   return
     // }
     // const authorizationToken = await user.getIdToken()
+    console.log("Getting other answers", nextIds)
+
     const [error, data] = await getOtherAnswers(nextIds)
 
     if (error) {
@@ -97,20 +99,27 @@ export const useAnswerFeed = (): UseAnswerFeedResults => {
     }
   }
 
+  // Fetch answer IDs on mount
   useEffect(() => {
     getOtherAnswerIDsHelper()
   }, [])
 
+  // Fetch initial batch on mount after answer IDs are fetched
   useEffect(() => {
     if (currentAnswerIndex === 0 && answerIds.length > 0) {
       loadNextBatch()
     }
   }, [answerIds])
 
-  const showNextAnswer = () => {
-    if (currentAnswerIndex === answerIds.length - 2 && hasMore) {
+  useEffect(() => {
+    // Fetch next batch if we're at the second to last answer and there are more answers to fetch
+    if (currentAnswerIndex === answers.length - 2 && hasMore) {
+      console.log("Loading next batch")
       loadNextBatch()
     }
+  }, [currentAnswerIndex])
+
+  const showNextAnswer = () => {
     setCurrentAnswerIndex(prev => prev + 1)
   }
 
